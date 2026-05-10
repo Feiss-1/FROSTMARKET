83dc9f17278f2e00e728a3785b8b097b763f3cf1
Feiss-1 <niki.o.jarvinen@gmail.com>
2026-05-10 12:24:32 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "FROSTMARKET! THE BEST BGL'S HERE!❄️",
+        name: "FROSTMARKET❄️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
